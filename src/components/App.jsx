2658dbdf24ca8586835b2e3b861910364a410216
Velsaal <ivanmitj@gmail.com>
2025-04-11 13:06:37 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
+  );
+};
+
+export default App;
